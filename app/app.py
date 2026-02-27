5d8071d9857f24e033db4f97a0def371f4dd7d69
coders-treasure <mucharlauha@gmail.com>
2026-02-27 13:25:32 +0530

    Testing automatic deployment

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project is Running Successfully 🚀"
+    return "CI/CD is Fully Automated 🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
